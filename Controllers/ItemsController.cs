ed7c05c29bc6bec57ba853b4d6d45de4d8ec4c71
Divya <divyakumarisingh546@gmail.com>
2023-09-02 12:36:08 +0530

    first commit

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using V_Selection.Models;
+
+namespace V_Selection.Controllers
+{
+    [Route("api/items")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateItem([FromBody] Item item)
+        {
+            // Here, you can process the received item data
+            // You can save it to a database, perform validations, etc.
+
+            // For simplicity, we'll return the received item as is.
+            return Ok(item);
+        }
+    }
+
+}
---
+++ Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace V_Selection.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int YearOfManufacture { get; set; }
+        public string Image { get; set; }
+    }
+
+}
